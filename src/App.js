6dbb56dbb50935eeb60c7be50b0ee6ed4bdc5792
mathenrifs <mateus_henrique2011@outlook.com>
2021-03-03 20:39:43 -0300

    add tasks

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ const App = () => {
     },
 ])
 
+// Add Task 
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id,...task}
+  setTasks([...tasks, newTask])
+}
+
 // Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id ))
@@ -40,11 +47,10 @@ const toggleReminder = (id) => {
   )
 }
 
-
   return (
     <div className="container">
         <Header />
-        <AddTask />
+        <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
         <Tasks tasks={tasks}
           onDelete={deleteTask} onToggle={toggleReminder} />
