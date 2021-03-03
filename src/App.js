be0cf869c1adf36c485ddd4f1d165cbf91ff2834
mathenrifs <mateus_henrique2011@outlook.com>
2021-03-02 21:33:26 -0300

    tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1, 
+        text: 'Doctor appointment', 
+        day: 'Feb 5th at 2:30pm', 
+        reminder: true,
+    },
+    {
+        id: 2, 
+        text: 'Meeting at School', 
+        day: 'Jan 8th at 2:30pm', 
+        reminder: true,
+    },
+    {
+        id: 3, 
+        text: 'Food Shoping', 
+        day: 'March 12th at 2:30pm', 
+        reminder: false,
+    },
+])
+
   return (
     <div className="container">
         <Header />
+        <Tasks tasks={tasks}/>
     </div>
   )
 } 
