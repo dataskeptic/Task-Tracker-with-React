2f6cac661afea00ddf85b726f695a55543255e2c
mathenrifs <mateus_henrique2011@outlook.com>
2021-03-08 23:43:51 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import About from './components/About'
 
 const App = () => {
 
-  const [showAddTaks, setShowAddTask] = useState(false)
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
 
   useEffect(() => {
@@ -63,9 +66,9 @@ const App = () => {
   const toggleReminder = async (id) => {
     const taskToToggle = await fetchTask(id)
     const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
-
     const res = await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'PUT',
+
       headers: {
         'Content-type': 'application/json'
       },
@@ -82,20 +85,30 @@ const App = () => {
   }
 
   return (
-    <div className="container">
-        <Header 
-          onAdd={() => setShowAddTask(!showAddTaks)}
-          showAdd={showAddTaks}/>
-        {showAddTaks && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks}
-          onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-          'No Tasks to show')}
-    </div>
+    <Router>
+      <div className="container">
+          <Header 
+            onAdd={() => setShowAddTask(!showAddTask)}
+            showAdd={showAddTask}
+          />
+        <Route path='/' exact render={(props) =>
+        (
+          <>
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ? (
+              <Tasks
+                tasks={tasks}
+                onDelete={deleteTask}
+                onToggle={toggleReminder} />
+            ) : (
+              'No Tasks to show')}
+          </>
+        )} />
+            <Route path='/about' component={About} />  
+          <Footer />
+      </div>
+    </Router>
   )
-} 
-
-
+}   
 
 export default App;
